e4f77c9c9a8e3b7f94ae1bd6748828de5e26d014
calebsli <444947389@qq.com>
2019-07-15 11:01:55 +0800

    finish

--- src/app/component/horizontal-grid/horizontal-grid.component.ts
+++ src/app/component/horizontal-grid/horizontal-grid.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./horizontal-grid.component.css']
 })
 export class HorizontalGridComponent implements OnInit {
-  username = ''
-  // private _username = '';
-  // @Output() usernameChange = new EventEmitter();
+  // username = ''
+  private _username = '';
+  @Output() usernameChange = new EventEmitter();
   constructor() {}
 
   ngOnInit() {}
@@ -17,13 +17,16 @@ export class HorizontalGridComponent implements OnInit {
    * get / set 是属性访问限定符，如果我们对于属性的读和写有一些逻辑操作
    * 可以利用 get / set 进行处理
    */
-  // @Input()
-  // public get username(): string {
-  //   return this._username;
-  // }
-
-  // public set username(value: string) {
-  //   this._username = value;
-  //   this.usernameChange.emit(value);
-  // }
+  // 第一次set函数里给_username赋值，走了一次get函数，因为和_username有关联，第二次父组件传回username又对_username赋值，又走了一次get函数，所以是两次。
+  @Input()
+  public get username(): string {
+    console.log('get', this._username)
+    return this._username;
+  }
+  
+  public set username(value: string) {
+    console.log('set value:', value)
+    this._username = value;
+    this.usernameChange.emit(value);
+  }
 }
