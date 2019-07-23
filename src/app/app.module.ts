293701695127e2db822f7935f8feeee5cc15ce1b
calebsli <444947389@qq.com>
2019-07-23 23:45:23 +0800

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './home'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, SharedModule],
+  imports: [BrowserModule, FormsModule, SharedModule, AppRoutingModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
